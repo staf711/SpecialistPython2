eac8b4937bcf0097f7d12601bdcce25fefd95a78
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-05 11:46:34 +0300

    Update BlackJack.py

--- Module9/practice/BlackJack/BlackJack.py
+++ Module9/practice/BlackJack/BlackJack.py
@@ -1,40 +1,66 @@
 # from cards import Deck, Card
 import json
-json.loads()
+from classes.Deck import BJDeck
+# json.loads()
 
 player_money = 100  # Деньги игрока
 rate_value = 10  # Размер ставки
-
-deck = Deck()
-
+def count_point(cards,points):
+    for card in cards:
+        points += card.point
+    return points
+deck = BJDeck()
+deck.shuffle()
 while True:
+    player_point = 0
+    rate_point = 0
     # 0. Игрок делает ставку
     player_money -= rate_value
     # 1. В начале игры перемешиваем колоду
     # 2. Игроку выдаем две карты
-    player_cards = ...
+    player_cards = deck.draw(2)
+    if player_cards == []: break
     # 3. Дилер берет одну карту
-    dealer_cards = ...
+    dealer_cards = deck.draw(1)
     # 4. Отображаем в консоли карты игрока и дилера
+    print(player_cards)
+    print(dealer_cards)
     # 5. Проверяем нет ли у игрока блэкджека (21 очко)
-    if blackjack:
-        # Выплачиваем выигрышь 3 и 2
+    player_point=count_point(cards=player_cards,points=player_point)
+    if player_point == 21:
+        print("This is Black Jack!!!")
         player_money += rate_value * 1.5
-        print("Black Jack!!! Игрок победил")
-        # Заканчиваем игру
-    # Если нет блэкджека, то
+        break
+    # # Если нет блэкджека, то
     while True:  # Игрок добирает карты пока не скажет "достаточно" или не сделает перебор (>21)
         player_choice = input("еще/достаточно: ")
         if player_choice.lower() == "еще" or player_choice.lower() == "ещё":
+            player_cards+=deck.draw(1)
+            print(player_cards)
+            player_point=count_point(cards=player_cards,points=player_point)
+            if player_point == 21:
+                print("This is Black Jack!!!")
+                player_money += rate_value * 1.5
+                break
+            elif player_point > 21:
+                print("Перебор :(")
+                break
             # Раздаем еще одну карту
             # Если перебор (>21), заканчиваем добор
             ...
         if player_choice.lower() == "достаточно":
+            dealer_cards+=deck.draw(1)
+            rate_point=count_point(cards=dealer_cards,points=rate_point)
+            if player_point == 21:
+                print("This is Black Jack!!!")
+                player_money += rate_value * 1.5
+                break
             # Заканчиваем добирать карты
+
             break
 
-    # Если у игрока не 21(блэкджек) и нет перебора, то
-    while True:  # дилер начинает набирать карты.
-        ...  # Смотри подробные правила добора дилера в задании
-
-    # Выясняем кто набрал больше очков. Выплачиваем/забираем ставку
+    # # Если у игрока не 21(блэкджек) и нет перебора, то
+    # while True:  # дилер начинает набирать карты.
+    #     ...  # Смотри подробные правила добора дилера в задании
+    #
+    # # Выясняем кто набрал больше очков. Выплачиваем/забираем ставку
